e281a6571ef9b588eeb0fe9ed5c11ca95c712493
SzymonLudwiniak <259215@student.pwr.edu.pl>
2023-01-30 17:33:58 +0100

    pre alpha v0.0.3.5 gui

--- src/main/java/com/vaultec/dbapp/gui/ContentPanel.java
+++ src/main/java/com/vaultec/dbapp/gui/ContentPanel.java
@@ -14,8 +14,12 @@ import com.vaultec.dbapp.gui.cards.ComplaintsCard;
 import com.vaultec.dbapp.gui.cards.LoginCard;
 import com.vaultec.dbapp.gui.cards.MainMenuCard;
 import com.vaultec.dbapp.gui.cards.WarehouseCard;
+import com.vaultec.dbapp.model.Dweller;
+import com.vaultec.dbapp.repository.DwellerRepository;
+import com.vaultec.dbapp.services.auth.LoginService;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
-
+import org.apache.commons.codec.digest.DigestUtils;
 /**
  * @author szymon
  */
@@ -27,6 +31,13 @@ public class ContentPanel extends JPanel {
 
 
     private void loginButtonAction(ActionEvent e) {
+        String[] loginData = LoginScreen.getLoginData();
+        Dweller dweller = service.verifyCredentials(loginData[0], DigestUtils.sha256Hex(loginData[1]));
+        if(dweller.equals(new Dweller())) {
+            System.out.println("LOGINSERVICE.VERIFYCRIDENTIALS FALIED");
+            return;
+        }
+
         CardLayout cl = (CardLayout)(this.getLayout());
         cl.show(this, "MainMenu");
     }
@@ -93,6 +104,17 @@ public class ContentPanel extends JPanel {
     private WarehouseCard Warehouse;
     private MainMenuCard MainMenu;
     private ComplaintsCard Complaints;
+
+    @Resource(name = "loginService")
+    private LoginService service;
+
+    public void setService(LoginService service) {
+        this.service = service;
+    }
+
+    public LoginService getService() {
+        return service;
+    }
 
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
